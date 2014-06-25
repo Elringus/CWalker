2933ab6727d13fa2a10a1629a06a4aa89858310b
Elringus <elringus@gmail.com>
2014-06-25 13:46:27 +0400

    implemented object placing

---
+++ Assets/_scripts/PlaceOnTerrain.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaceOnTerrain : MonoBehaviour
+{
+	private void Start () 
+	{
+		// places the object on the terrain surface and randomizes its y-rotation
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position, Vector3.down, out hit))
+		{
+			transform.position = hit.point;
+			transform.eulerAngles = new Vector3(0, Random.Range(-180, 180), 0);
+		}
+	}
+
+}
